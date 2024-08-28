a1b1e78a3fc79314fb560edb9106ba7bdd4ba287
Numbernaut <103599762+Numbernaut@users.noreply.github.com>
2024-08-29 01:05:35 +0200

    Update muteClickSound.js

--- muteClickSound.js
+++ muteClickSound.js
@@ -1,16 +1,3 @@
-if (typeof Game !== 'undefined') {
-    // Register the mod and ensure compatibility with the current game version
-    Game.registerMod("MuteClickSound", {
-        init: function () {
-            // Overriding the cookie click sound function to do nothing
-            Game.playCookieClickSound = function () { return; };
-
-            // Notify that the mod has been loaded
-            if (Game.prefs.popups) {
-                Game.Popup('MuteClickSound mod loaded!');
-            } else {
-                Game.Notify('MuteClickSound mod loaded!', '', '', 1, 1);
-            }
-        }
-    });
-}
+(function() {
+    console.log("Minimal script loaded!");
+})();
